b391b9ee344f8bba814c82b2a237e009c37796d6
AbhishekGag <137916055+AbhishekGag@users.noreply.github.com>
2023-07-12 15:20:07 +0530

    Create index.html

---
+++ emotion_detection.py
@@ -0,0 +1,25 @@
+"""Function for analyse text"""
+
+import json
+import requests
+
+def emotion_detector(text_to_analyse):
+    """Analyse the input text"""
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+        }
+    myobj = {
+        'raw_document': {'text': text_to_analyse}
+    }
+    response = requests.post(url, json=myobj, headers=header)
+    formatted_response = json.loads(response.text)
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    dominant_emotion = ''
+    max_score = -1.0
+    for emotion, score in emotions.items():
+        if score > max_score:
+            max_score = score
+            dominant_emotion = emotion
+    emotions['dominant_emotion'] = dominant_emotion
+    return emotions
