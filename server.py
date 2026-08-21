44f635f74915aa4d1dadf1a69ae8e57074d3a8d5
iltramont <luca.tramonti@outlook.it>
2026-08-21 12:28:12 -0400

    final commit

--- server.py
+++ server.py
@@ -17,21 +17,24 @@ def emot_detector():
     text_to_analyse = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyse)
     dominant_emotion = response.get('dominant_emotion')
-    response.pop('dominant_emotion')
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!"
+    response.pop("dominant_emotion")
     response_strings = []
     for emotion, score in response.items():
-        if emotion != 'sadness':
+        if emotion != "sadness":
             response_strings.append(f"'{emotion}': {score}")
-    response_string = ', '.join(response_strings)
+    response_string = ", ".join(response_strings)
     response_string += f" and 'sadness': {response['sadness']}"
-    return f"For the given statement, the system response is {response_string}. The dominant emotion is {dominant_emotion}"
+    return f"For the given statement, the system response is {response_string}." \
+           f" The dominant emotion is {dominant_emotion}."
 
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
-    return render_template('index.html')
+    return render_template("index.html")
 
 if __name__ == "__main__":
     # This functions executes the flask app and deploys it on localhost:5000
